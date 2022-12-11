c821d09770e9fd0da3978daf90c1ff56fc0ec6f4
Ceyda Sahin <ceydasahin889@gmail.com>
2022-12-12 00:14:52 +0300

    Chat Bar added
    Chat bar with popup, and a fun little detail (speech bubble) was added to the messaging home page.

---
+++ src/frontend/src/Components/SpeechBubbleOther.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SpeechBubble.css"
+
+function SpeechBubble() {
+  return (
+    <div>
+        <p>If you have one bucket that contains 2 gallons and another bucket that contains 7 gallons, how many buckets do you have?</p>
+    </div>
+  )
+}
+
+export default SpeechBubble
--- src/frontend/src/Pages/HomePage.jsx
+++ src/frontend/src/Pages/HomePage.jsx
@@ -3,9 +3,14 @@ import { useState } from "react"
 import { OffcanvasBody } from "react-bootstrap"
 import Button from "react-bootstrap/Button"
 import Offcanvas from "react-bootstrap/Offcanvas"
-
+import Badge from 'react-bootstrap/Badge';
+import "./HomePage.css"
 import io from "socket.io-client"
 import ChatPreviews from "./ChatPreviews"
+import SpeechBubble from "../Components/SpeechBubbleOther"
+
+import ChatBarPrivate from "../Components/ChatBarPrivate"
+import ChatBarGroup from "../Components/ChatBarGroup"
 
 const connection = io("http://localhost:3001")
 var id = -1
@@ -17,7 +22,6 @@ connection.on("connect", (socket) => {
 function HomePage() {
 	const [offcanvasVisible, setOffcanvasVisible] = useState(false)
 	const [messages, setMessages] = useState([])
-
 	const [typingMessage, setTypingMessage] = useState("")
 
 	function onMessageSend(e) {
@@ -41,14 +45,21 @@ function HomePage() {
 
 	return (
 		<div>
-			<Button variant="primary" onClick={() => setOffcanvasVisible(true)}>
-				Open
+			<Button className="btn-show-chats" variant="outline-primary" onClick={() => setOffcanvasVisible(true)}>
+				Show Chats {''} <Badge bg="success">1</Badge>
 			</Button>
-			<Offcanvas show={offcanvasVisible} backdrop="false" onHide={() => setOffcanvasVisible(false)}>
+      <hr/>
+
+      <SpeechBubble></SpeechBubble>
+      
+			<Offcanvas  show={offcanvasVisible} backdrop="false" onHide={() => setOffcanvasVisible(false)}>
 				<Offcanvas.Header closeButton>
 					<Offcanvas.Title>Disturd 💩</Offcanvas.Title>
 				</Offcanvas.Header>
         <OffcanvasBody>
+		<div style={{display:'flex', gap:'10px'}}><ChatBarPrivate/> <ChatBarGroup/></div>
+		<hr/>
+
         <div style={{display:'flex'}}><ChatPreviews></ChatPreviews></div>
         </OffcanvasBody>
 			</Offcanvas>
