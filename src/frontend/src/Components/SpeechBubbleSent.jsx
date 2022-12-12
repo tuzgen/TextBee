e1e4c615af0083c59f96396fb7d65f302b282b64
Ceyda Sahin <ceydasahin889@gmail.com>
2022-12-12 03:23:09 +0300

    Redundancies removed

--- src/frontend/src/Components/SpeechBubbleSent.jsx
+++ src/frontend/src/Components/SpeechBubbleSent.jsx
@@ -4,11 +4,11 @@ import "./SpeechBubble.css"
 function SpeechBubbleSent(props) {
   return (
     <div style={{ position:'absolute', right:'10px'}}>
-      <div style={{marginLeft:'10px'}}> <h1 style={{fontSize:'medium', fontWeight:'800'}}>{props.name}li</h1></div>
+      <div style={{marginLeft:'10px'}}> <h1 style={{fontSize:'medium', fontWeight:'800'}}>{props.name}</h1></div>
 
       <div style={{display:'flex', alignItems:'center', gap:'20px'}}>
 
-      <p>{props.message}li</p>
+      <p>{props.message}</p>
       <h2 style={{fontSize:'medium', fontWeight:'500'}}>20:03</h2>
       </div>
     
